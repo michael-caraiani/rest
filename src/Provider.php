6a3ef1f5002e1d7979bae19dcb95c5bbd50d38ed
Nikita Chernyi <developer.nikus@gmail.com>
2017-10-19 15:37:09 +0300

    Added user context into container on success auth, closes #1

--- src/Provider.php
+++ src/Provider.php
@@ -6,6 +6,8 @@ namespace TiSuit\Rest;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Tuupola\Middleware\JwtAuthentication;
 
 class Provider implements ServiceProviderInterface
@@ -14,6 +16,13 @@ class Provider implements ServiceProviderInterface
     {
         $container['jwt_middleware'] = function ($container) {
             $config = $container['config']('jwt', []);
+            if (!($config['before'] ?? null)) {
+                $config['before'] = function (ServerRequestInterface $request, ResponseInterface $response, array $args) use ($container) {
+                    $container['user'] = (property_exists($request->getAttribute('token'), 'data') ? $request->getAttribute('token')->data : $request->getAttribute('token'));
+
+                    return $request;
+                };
+            }
 
             return new JwtAuthentication($config);
         };
--- tests/ProviderTest.php
+++ tests/ProviderTest.php
@@ -22,5 +22,10 @@ class ProviderTest extends TestCase
     public function testJwtAuthentication(): void
     {
         $this->assertInstanceOf('\Tuupola\Middleware\JwtAuthentication', $this->container->jwt_middleware);
+        $request = $this->container->request->withHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ');
+        $response = $this->container->response;
+        $callable = function ($request, $response) { return $response; };
+        $result = $this->container->jwt_middleware->__invoke($request, $response, $callable);
+        $this->assertArrayHasKey('name', (array) $this->container->user);
     }
 }
