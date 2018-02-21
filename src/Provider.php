09ce450f1b79ec6be825c020fed753a77e48e7dc
michael-caraiani <33863860+michael-caraiani@users.noreply.github.com>
2018-02-21 14:21:53 +0200

    Update 'tuupola/slim-jwt-auth' to 3.0.0-rc.5
    applying the following patch to fix tests

--- src/Provider.php
+++ src/Provider.php
@@ -17,8 +17,9 @@ class Provider implements ServiceProviderInterface
         $container['jwt_middleware'] = function ($container) {
             $config = $container['config']('jwt', []);
             if (!($config['before'] ?? null)) {
-                $config['before'] = function (ServerRequestInterface $request, ResponseInterface $response, array $args) use ($container) {
-                    $container['user'] = (property_exists($request->getAttribute('token'), 'data') ? $request->getAttribute('token')->data : $request->getAttribute('token'));
+                $config['before'] = function (ServerRequestInterface $request, array $args) use ($container) {
+                    $token = $request->getAttribute('token');
+                    $container['user'] = (is_object($token) && property_exists($token, 'data') ? $token->data : $token);
 
                     return $request;
                 };
